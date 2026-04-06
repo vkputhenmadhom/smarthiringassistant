9d631793dba6b17380ea8150ba660b876a1c89c5
vinodputhenmadhom <vkputhenmadhom@gmail.com>
2026-04-06 09:19:07 -0500

    removing unused methods

--- services/job-analyzer-service/src/main/java/org/vinod/sha/analyzer/repository/JobAnalysisRepository.java
+++ services/job-analyzer-service/src/main/java/org/vinod/sha/analyzer/repository/JobAnalysisRepository.java
@@ -4,13 +4,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 import org.vinod.sha.analyzer.entity.JobAnalysis;
 
-import java.util.List;
-import java.util.Optional;
-
 @Repository
 public interface JobAnalysisRepository extends MongoRepository<JobAnalysis, String> {
-    Optional<JobAnalysis> findByJobId(String jobId);
-    List<JobAnalysis> findByJobTitleContainingIgnoreCase(String jobTitle);
-    List<JobAnalysis> findByLocationContainingIgnoreCase(String location);
 }
 
